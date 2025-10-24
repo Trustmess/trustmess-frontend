81e41211f2fea5c5b0dcf0233fb68489c7d5ab59
Stanislav Kucherenko <svkwork1@gmail.com>
2025-10-25 00:51:02 +0200

    feat: implement user fetching in MessengerPage; add ContactCard component; enhance styles for contacts panel

--- src/pages/MessengerPage.jsx
+++ src/pages/MessengerPage.jsx
@@ -1,6 +1,8 @@
 import { useTheme } from '@contexts/ThemeContext';
-import { Button } from '@components';
+import { Button, ContactCard } from '@components';
 import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getUsers } from '/src/api/requests';
 // React-icons
 import { MdDarkMode, MdLightMode } from 'react-icons/md';
 
@@ -9,7 +11,29 @@ import '/src/scss/_pages/_messenger_page.scss';
 
 export const MessengerPage = () => {
   const { theme, toggleTheme } = useTheme();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const allUsers = await getUsers();
+        setUsers(allUsers);
+      } catch (error) {
+        console.log('Error loading users:', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className='messenger_page'>
       <nav className='nav_line' id='navLine'>
@@ -28,7 +52,11 @@ export const MessengerPage = () => {
       </nav>
 
       <main className='messenger_container'>
-        <div className='contacts_panel'></div>
+        <div className='contacts_panel'>
+          {users.map((user) => (
+            <ContactCard key={user.id} userName={user.username} />
+          ))}
+        </div>
         <div className='dialog_panel'></div>
       </main>
     </div>
